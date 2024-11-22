1de1d2e0485c9bbfedd1a92a4bc2f554bb063f95
YongJun Cho <whdydwns73@naver.com>
2024-11-22 15:00:37 +0900

    first commit

---
+++ src/test/java/com/devsign/member/MemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsign.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
